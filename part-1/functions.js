189ecb032cfdb874c1619148a5a2d30b08d9e29b
MsMishi <mcwcharles@gmail.com>
2017-06-19 22:44:57 -0700

    Add more CSS

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,9 +1,39 @@
-let month = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+//let month = [
+//  'January',
+//  'February',
+//  'March',
+//  'April',
+//  'May',
+//  'June',
+//  'July',
+//  'August',
+//  'September',
+//  'October',
+//  'November',
+//  'December'
+//]
+//
+//let date = new Date(2017, 5, 19)
+//
+//function weekday(date) {
+//  let day = [
+//    'Mon',
+//    'Tue',
+//    'Wed',
+//    'Thu',
+//    'Fri',
+//    'Sat',
+//    'Sun'
+//  ]
+//
+//}
+//weekday(new Date(2017, 5, 15))
+//
 
-let date = new Date(2017, 5, 19)
-
-function weekday(date) {
-  let day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-
+function snippet(string, maxlength){
+  if (string > maxlength) {
+    return string.substring(0, maxlength - 3) + '...'
+  } else {
+    return string
+  }
 }
-weekday(new Date(2017, 5, 15))
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,7 +1,29 @@
-
-describe ('')
-describe ('Determine day of the Week', function () {
-  it('returns day of the week for a given date', function(){
+/*describe('')
+describe('Determine day of the Week', function() {
+  it('returns day of the week for a given date', function() {
     expect(weekday).toBe(month + ' ', + day + ', ' + year)
+  })
+})
+*/
+
+describe('return snippet or string', function() {
+  it('should return snippet', function() {
+    var string = 'Hello world!';
+    var maxlength = 5;
+    function snippet(string, maxlength) {
+      if (string > maxlength) {
+        return string.substring(0, maxlength - 3) + '...'
+      };
+      expect(snippet()).toEqual('He...')
+    }) it('should return string', function() {
+      var string = 'Hello world';
+      var maxlength = 15;
+      function snippet(string, maxlength) {
+        if (string < maxlength) {
+          return string
+        };
+        expect(snippet()).toEqual('Hello world!')
+      }
+    })
   })
 })
