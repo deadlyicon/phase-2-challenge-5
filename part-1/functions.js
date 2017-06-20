480e4cc1da5f6f2453f01a6bb6071c6df2537004
MsMishi <mcwcharles@gmail.com>
2017-06-19 17:28:33 -0700

    Add HTML and CSS for online grocer

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,8 +1,9 @@
-let day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 let month = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 let date = new Date(2017, 5, 19)
 
 function weekday(date) {
+  let day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 }
+weekday(new Date(2017, 5, 15))
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,7 +1,7 @@
 
 describe ('')
 describe ('Determine day of the Week', function () {
-  it('finds day of the week for a given date', function(){
-    expect(year).toBe(integer)
+  it('returns day of the week for a given date', function(){
+    expect(weekday).toBe(month + ' ', + day + ', ' + year)
   })
 })
